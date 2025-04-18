40690cfabc7436820854e0a5d78db6290ab3d470
Tina <nosratitina@gmail.com>
2025-04-18 15:47:19 -0400

    pin created

--- public/places.js
+++ public/places.js
@@ -10,24 +10,34 @@ const deletePlace = async (id) => {
     await loadPlaces();
 };
 
-let map = L.map('map').setView([41, -74], 13);
+// Initialize map
+let map = L.map('map').setView([41.08, -74.17], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Store markers globally to manage them
 let markers = [];
 
 const loadPlaces = async () => {
     const response = await axios.get('/places');
     const tbody = document.querySelector('tbody');
 
-    while (tbody.firstChild) tbody.removeChild(tbody.firstChild);
-    markers.forEach(marker => map.removeLayer(marker));
+    // Clear table
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+
+    // Remove old markers from map
+    for (let i = 0; i < markers.length; i++) {
+        map.removeLayer(markers[i]);
+    }
     markers = [];
 
     if (response.data.places) {
         for (const place of response.data.places) {
+            // Update table row
             const tr = document.createElement('tr');
             tr.innerHTML = `
                 <td>${place.label}</td>
@@ -36,9 +46,11 @@ const loadPlaces = async () => {
             `;
             tbody.appendChild(tr);
 
+            // Add marker if lat/lng are valid
             if (place.lat && place.lng) {
-                const marker = L.marker([place.lat, place.lng]).addTo(map);
-                marker.bindPopup(`<strong>${place.label}</strong><br>${place.address}`);
+                const marker = L.marker([place.lat, place.lng])
+                    .addTo(map)
+                    .bindPopup(`<b>${place.label}</b><br/>${place.address}`);
                 markers.push(marker);
             }
         }
